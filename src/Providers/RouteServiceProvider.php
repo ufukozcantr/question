491e7e40e7f5c1fb678e73fd51d66ac2b162e7d7
ufukozcantr <ufukozcantr@gmail.com>
2019-05-08 15:06:53 +0300

    routeserviceprovider

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Question\Providers;
+namespace ufukozcantr\Question\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
